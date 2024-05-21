d641eeb055061135a92cc2c358d03e148d952b26
GILANAH <hopegilana@gmail.com>
2024-05-21 12:26:27 +0300

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+export default function App() {
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <form className="new-item-form">
+      <div className="form-row">
+
+        <label htmlFor="item">New item</label>
+        <input type="text" id="item" />
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </form>
+    <h1 className="header">Todo list</h1>
+    <ul className="list">
+      <li>
+        <label>
+          <input type="checkbox"/>
+          item 1
+        </label>
+        <button className="delete-btn">delete</button>
+      </li>
+    </ul>
     </>
   )
+  
 }
-
-export default App
