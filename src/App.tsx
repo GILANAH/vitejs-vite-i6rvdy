0e4f3ed3345b7862b11dd9fe5f6ed087d260efc8
GILANAH <hopegilana@gmail.com>
2024-05-24 19:39:22 +0300

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
 import "./App.css"
 
-export default function App() {
+export default function App(){
   return(
     <>
-    <form className="new-item-form">
-      <div className="form-row">
+    <form>
+<div className="form-row">
 
-        <label htmlFor="item">New item</label>
-        <input type="text" id="item" />
+  <label htmlFor="new-item" >
+    <input type="text" id="new-item" />new item
+  </label>
+</div>
 
-      </div>
     </form>
-    <h1 className="header">Todo list</h1>
-    <ul className="list">
-      <li>
-        <label>
-          <input type="checkbox"/>
-          item 1
-        </label>
-        <button className="delete-btn">delete</button>
-      </li>
-    </ul>
+    
     </>
   )
-  
 }
